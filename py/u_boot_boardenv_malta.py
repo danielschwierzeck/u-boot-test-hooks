3c3e85aa33a7adc98782ce1fec77b12c9d8d1900
Daniel Schwierzeck <daniel.schwierzeck@gmail.com>
2016-07-05 20:09:12 +0200

    Add test environment for MIPS Malta board

---
+++ py/u_boot_boardenv_malta.py
@@ -0,0 +1,13 @@
+#
+# SPDX-License-Identifier: MIT
+#
+# Test environment for MIPS Malta board
+#
+
+env__net_dhcp_server = True
+
+#env__net_tftp_readable_file = {
+#    "fn": "ubtest.bin",
+#    "size": 1048576,
+#    "crc32": "c4d4fe09",
+#}
