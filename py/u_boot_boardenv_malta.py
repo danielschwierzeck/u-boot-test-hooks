d63669145bbea7b4a010c289d565c739825ea7fb
Daniel Schwierzeck <daniel.schwierzeck@gmail.com>
2016-09-14 22:46:10 +0200

    Make network tests working

--- py/u_boot_boardenv_malta.py
+++ py/u_boot_boardenv_malta.py
@@ -4,10 +4,16 @@
 # Test environment for MIPS Malta board
 #
 
-env__net_dhcp_server = True
+env__net_uses_pci = True
+env__net_dhcp_server = False
 
-#env__net_tftp_readable_file = {
-#    "fn": "ubtest.bin",
-#    "size": 1048576,
-#    "crc32": "c4d4fe09",
-#}
+env__net_static_env_vars = [
+    ("ipaddr", "10.0.2.15"),
+    ("serverip", "10.0.2.2"),
+]
+
+env__net_tftp_readable_file = {
+    "fn": "ubtest.bin",
+    "size": 1048576,
+    "crc32": "c4d4fe09",
+}
--- py/u_boot_boardenv_malta64.py
+++ py/u_boot_boardenv_malta64.py
@@ -4,8 +4,15 @@
 # Test environment for MIPS Malta board
 #
 
-env__net_dhcp_server = True
+env__net_uses_pci = True
+env__net_dhcp_server = False
 
+env__net_static_env_vars = [
+    ("ipaddr", "10.0.2.15"),
+    ("serverip", "10.0.2.2"),
+]
+
+# there are still TFTP timeout issues
 #env__net_tftp_readable_file = {
 #    "fn": "ubtest.bin",
 #    "size": 1048576,
